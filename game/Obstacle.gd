4c2f354571ee72ab39d4d922ad4b2828f3ef8a8f
Jake Deerin <2016deerinjo@gmail.com>
2023-12-16 14:19:26 -0500

    improved picking up objects

--- game/Obstacle.gd
+++ game/Obstacle.gd
@@ -4,6 +4,7 @@ extends StaticBody3D
 @export var growth_amount: float = 0.
 
 var _initial_scale: Vector3
+var _initial_pos: Vector3
 var _pickup_size: float = 0.0
 var _player: Node3D
 
@@ -20,14 +21,16 @@ func player_collision(player):
 		player.reset_position()
 	elif not is_picked_up:
 		is_picked_up = true
-		_player = player
-		_pickup_size = player.size
 		player.grow(growth_amount)
 		self.reparent(player.get_node("Snowball"))
 		_initial_scale = scale
+		_initial_pos = position
+		_player = player
+		_pickup_size = player.size
 		growth_amount = 0
 		
 func _process(delta):
 	if is_picked_up:
-		pass
-		# scale = _initial_scale * (_pickup_size)/_player.size
+		var adjusted_scale = (_pickup_size)/_player.size
+		scale = _initial_scale * adjusted_scale
+		position = _initial_pos * adjusted_scale
